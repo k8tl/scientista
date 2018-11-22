16812689f9b72a3a84b377a4a0d4ba3bd4f7a9a3
Rylen Sampson <16ras6@queensu.ca>
2018-11-22 18:00:40 -0500

    adding

--- header.php
+++ header.php
@@ -8,7 +8,6 @@ global $template_settings, $post;
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, minimal-ui=0">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" integrity="sha384-3AB7yXWz4OeoZcPbieVW64vVXEwADiYyAEhwilzWsLw+9FgqpyjjStpPnpBO8o8S" crossorigin="anonymous">
-
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
--- page-templates/front-page.php
+++ page-templates/front-page.php
@@ -7,11 +7,33 @@ get_header();
 <div class='container-fluid background-texture h-100'>
   <div class="row h-100">
     <div class="col-sm-12">
-      <div class="text-block"><h1>Welcome To QWeb</h1>
-      <p>This theme is a starting point for wordpress theme development. It is built with Bootstrap and Wordpress. In order to work with this theme it is highly recommended that you understand how to use and compile SASS.</p>
-      </div>
+
     </div>
   </div>
 </div>
 </header>
+<section class="first-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <h2>We made this section</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-4">
+        <h3>first</h3>
+      </div>
+      <div class="col-lg-4">
+        <h3>second</h3>
+
+      </div>
+      <div class="col-lg-4">
+        <h3>third</h3>
+
+      </div>
+    </div>
+  </div>
+
+
+</section>
 <?php get_footer(); ?>
