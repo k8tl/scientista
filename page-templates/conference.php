298c3f1b65e0110d5cc31d57f2d23ce0dfe6d828
Katie <17kygl@queensu.ca>
2018-11-27 11:06:48 -0500

    added conference page

---
+++ page-templates/conference.php
@@ -0,0 +1,40 @@
+<?php
+global $template_settings, $post;
+bb_add_body_classes('front-page full-height');
+the_post();
+get_header();
+?>
+<div class='container-fluid background-texture h-100'>
+  <div class="row h-100">
+    <div class="col-sm-12">
+
+    </div>
+  </div>
+</div>
+</header>
+<section class="first-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <h2>Conference</h2>
+        <p>Details TBA</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-4">
+        <h3>first</h3>
+      </div>
+      <div class="col-lg-4">
+        <h3>second</h3>
+
+      </div>
+      <div class="col-lg-4">
+        <h3>third</h3>
+
+      </div>
+    </div>
+  </div>
+
+
+</section>
+<?php get_footer(); ?>
