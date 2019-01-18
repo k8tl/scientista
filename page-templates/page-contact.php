cd4eef52d36c2745a225a015dafdc9e564cdc348
Patrick Tsapoitis <patrick.tsapoitis@gmail.com>
2019-01-18 18:23:16 -0500

    Updated contact page

--- page-templates/page-contact.php
+++ page-templates/page-contact.php
@@ -3,16 +3,17 @@ global $template_settings, $post;
 bb_add_body_classes('Contact');
 the_post();
 get_header();
+get_footer();
 <body>
 <h1> Contact Us </h1>
 <div class="container">
   <form action="action_page.php">
 
     <label for="fname">First Name</label>
-    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+    <input type="text" id="fname" name="firstname" placeholder="Your name">
 
     <label for="lname">Last Name</label>
-    <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+    <input type="text" id="lname" name="lastname" placeholder="Your last name">
 
     <label for="Inquiries">Subject</label>
     <textarea id="Inquries" name="Inquiries" placeholder="Write something.." style="height:200px"></textarea>
