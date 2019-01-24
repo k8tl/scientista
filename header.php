3cc212a1eca15abb216e0c795ec4353340cb7019
Rasamps <16ras6@queensu.ca>
2019-01-24 16:08:08 -0500

    testing

--- header.php
+++ header.php
@@ -13,7 +13,7 @@ global $template_settings, $post;
 <body <?php body_class(); ?>>
   <header>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Scientista</a>
+    <a class="navbar-brand" href="#">Scientista Ryl</a>
     <button class="navbar-toggler hamburger hamburger--collapse" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" onclick="navToggle()">
       <span class="hamburger-box">
         <span class="hamburger-inner"></span>
