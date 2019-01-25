45c8430932b1065e0b1431ae939ff7f0216e5029
Rasamps <16ras6@queensu.ca>
2019-01-25 15:00:10 -0500

    Adding content

--- page-templates/mission.php
+++ page-templates/mission.php
@@ -7,17 +7,41 @@ get_header();
 ?>
 
 <head>
-
-
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+	<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet'>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="Styles.css">
+  <title> Scientista's Mission </title>
+  <style>
+    @font-face {
+        font-family: 'playlistscript';
+        src: url('playlist_script-webfont.woff2') format('woff2'),
+          url('playlist_script-webfont.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+      }
+    body {background-color: #d2d1ff;
+        font-family: 'playlistscript';}
+  </style>
 </head>
 
-<h1>
-  Gallery
-</h1>
-
-<p>
-  Queen's University Scientista is the first Canadian Chapter of the Scientista Foundation (http://www.scientistafoundation.com/) to be found in 2015. The aim is to promote the advancement of undergraduate and graduate women in science, technology, engineering, mathematics, and medicine (STEMM) on Queen's University’s campus through campus events and campus-related resources as well as by connecting women to a larger network of women in STEMM. Queen's University Scientista strives to provide women in STEMM with the resources and support necessary to help them succeed in their current and future endeavors. We will do this through publication of online content, posting of science resources and opportunities, hosting campus events to build the Scientista community, and engaging with women in STEMM at a national level.
-</p>
+<body>
+  <div class = "container-fluid" style = "background-color: #6c4f99">
+    <h1 align = "left";
+    style = "padding-top: 15px;
+        padding-bottom: 15px;
+        padding-left: 20px
+        font-family: playlistscript;
+        color: white">
+      Scientista
+    </h1>
+  </div>
+  <p>
+    Queen's University Scientista is the first Canadian Chapter of the Scientista Foundation (http://www.scientistafoundation.com/) to be found in 2015. The aim is to promote the advancement of undergraduate and graduate women in science, technology, engineering, mathematics, and medicine (STEMM) on Queen's University’s campus through campus events and campus-related resources as well as by connecting women to a larger network of women in STEMM. Queen's University Scientista strives to provide women in STEMM with the resources and support necessary to help them succeed in their current and future endeavors. We will do this through publication of online content, posting of science resources and opportunities, hosting campus events to build the Scientista community, and engaging with women in STEMM at a national level.
+  </p>
+</body>
 
 <?php get_footer(); ?>
 </html>
