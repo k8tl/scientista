cd5e1c50b5447a953a83578e4f5f041ddb0e2928
Katie Lu <katie.yg.lu@gmail.com>
2019-04-06 15:31:32 -0400

    made front page

--- page-templates/front-page.php
+++ page-templates/front-page.php
@@ -4,36 +4,74 @@ bb_add_body_classes('front-page full-height');
 the_post();
 get_header();
 ?>
-<div class='container-fluid background-texture h-100'>
-  <div class="row h-100">
-    <div class="col-sm-12">
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
 
-    </div>
-  </div>
-</div>
-</header>
-<section class="first-section">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-12">
-        <h2>We made this section</h2>
+    <title>Jumbotron Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="jumbotron.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <main role="main">
+
+      <!-- Main jumbotron for a primary marketing message or call to action -->
+      <div class="jumbotron" style="background-image: url(https://lh3.googleusercontent.com/S-kuv9OwZm23IaFAhfC1ipODG1CqvUtWfix1Yc8mVdDPbNPKoVQECdeAeDuPY6TdlJzDweIBOaJ8w0nXWGSbD6bjrtC8NwAKeCHYsOL-QHLuRj3C5712MyvaTrbmWmm3_qjEg0P8); background-size: cover">
+        <div class="container">
+          <!--<h1 class="display-3" style="font-family: Playlist Script">Queen's University Scientista</h1>
+          <p>Empowering Women in STEMM.</p>-->
+          <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+        </div>
       </div>
-    </div>
-    <div class="row">
-      <div class="col-lg-4">
-        <h3>first</h3>
-      </div>
-      <div class="col-lg-4">
-        <h3>second</h3>
 
-      </div>
-      <div class="col-lg-4">
-        <h3>third</h3>
+      <div class="container">
+        <!-- Example row of columns -->
+        <div class="row">
+          <div class="col-md-4">
+            <h2>Podcast: Women of Scientista</h2>
+            <p>"Women of Scientista" is a social media initiative with the goal of highlighting a women in STEMM on Queen's campus, including professors, researchers and graduate students.</p>
+            <p><a class="btn btn-secondary" href="https://www.podomatic.com/podcasts/queensscientista" role="button">Listen here! &raquo;</a></p>
+          </div>
+          <div class="col-md-4">
+            <h2>Heading</h2>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+          </div>
+          <div class="col-md-4">
+            <h2>Heading</h2>
+            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+          </div>
+        </div>
 
-      </div>
-    </div>
-  </div>
+        <hr>
 
+      </div> <!-- /container -->
 
-</section>
+    </main>
+
+    <footer class="container">
+      <p>&copy; Scientista 2017-2018</p>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
+
 <?php get_footer(); ?>
