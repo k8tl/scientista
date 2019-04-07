c408d9a496f371d0c1ea81bab0951036d274f097
Katie Lu <katie.yg.lu@gmail.com>
2019-04-07 13:38:36 -0400

    added headshots and descriptions

--- page-templates/page-team.php
+++ page-templates/page-team.php
@@ -4,3 +4,146 @@ bb_add_body_classes('team');
 the_post();
 get_header();
 ?>
+<style>
+h2 {
+    font-family: Playlist Script
+    }
+</style>
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="https://scontent.fybz2-2.fna.fbcdn.net/v/t1.0-9/43514596_1970477453044960_666739152389144576_n.jpg?_nc_cat=104&_nc_ht=scontent.fybz2-2.fna&oh=bd279bef842c4d299d7c652e30dbe8c8&oe=5D04E75F" alt="Maria" style="width:20%">
+      <div class="container">
+        <h2>Maria Shaheen</h2>
+        <p class="title">Co-Director</p>
+        <p>4th Year Life Sciences</p>
+        <p>example@example.com</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="https://scontent.fybz2-2.fna.fbcdn.net/v/t1.0-9/43604459_1970477359711636_6706792243899924480_n.jpg?_nc_cat=103&_nc_ht=scontent.fybz2-2.fna&oh=a2f40b4bad204a11bddafacee0c1233b&oe=5D505B0F" alt="Britney Jeyanayagam" style="width:20%">
+      <div class="container">
+        <h2>Britney Jeyanayagam</h2>
+        <p class="title">Co-Director</p>
+        <p>4th Year Life Sciences</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="https://scontent.fybz2-1.fna.fbcdn.net/v/t1.0-9/43490619_1970477346378304_1357049579219976192_n.jpg?_nc_cat=110&_nc_ht=scontent.fybz2-1.fna&oh=c9a7bb96a4a74b40fcc6eff62a12b2a1&oe=5D394174" alt="Brooke Clouston" style="width:20%">
+      <div class="container">
+        <h2>Brooke Clouston</h2>
+        <p class="title">Treasurer</p>
+        <p>3rd Year Computer Science specializing in Math</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="https://scontent.fybz2-2.fna.fbcdn.net/v/t1.0-9/43514606_1970477376378301_7669715326023499776_n.jpg?_nc_cat=104&_nc_ht=scontent.fybz2-2.fna&oh=5a77355fc0ad6955a89f650e943570a5&oe=5D3EBBAC" alt="Lena Malizia-Kelly" style="width:20%">
+      <div class="container">
+        <h2>Lena Malizia-Kelly</h2>
+        <p class="title">PR and Marketing Chair</p>
+        <p>3rd Year Mathematics</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="https://scontent.fybz2-2.fna.fbcdn.net/v/t1.0-9/43541424_1970477539711618_9053794676330463232_n.jpg?_nc_cat=102&_nc_ht=scontent.fybz2-2.fna&oh=83534726da67edfcda0cb9c079753194&oe=5D409ED3" alt="Mikayla Erdelsky" style="width:20%">
+      <div class="container">
+        <h2>Mikayla Erdelsky</h2>
+        <p class="title">PR and Marketing Chair</p>
+        <p>3rd Year Life Science SSP (Neuroscience)</p>
+      </div>
+    </div>
+  </div>
+    
+  <div class="column">
+    <div class="card">
+      <img src="https://scontent.fybz2-2.fna.fbcdn.net/v/t1.0-9/43573206_1970477413044964_3791543076745379840_n.jpg?_nc_cat=107&_nc_ht=scontent.fybz2-2.fna&oh=fbf2a41950c441a5d2269a31796a9c86&oe=5D411255" alt="Claire Pao" style="width:20%">
+      <div class="container">
+        <h2>Claire Pao</h2>
+        <p class="title">Graphics Chair</p>
+        <p>4th Year Life Sciences and Psychology</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="https://scontent.fybz2-2.fna.fbcdn.net/v/t1.0-9/43705673_1970477509711621_1296926406512476160_n.jpg?_nc_cat=105&_nc_ht=scontent.fybz2-2.fna&oh=a8a85aa640344e4bc209b144f6799d6a&oe=5D4DAB34" alt="Megan Houzer" style="width:20%">
+      <div class="container">
+        <h2>Megan Houzer</h2>
+        <p class="title">Graphics Chair</p>
+        <p>3rd Year Life Sciences</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+
+
+  <div class="column">
+    <div class="card">
+      <img src="https://scontent.fybz2-2.fna.fbcdn.net/v/t1.0-9/43565410_1970477439711628_9011813531380613120_n.jpg?_nc_cat=104&_nc_ht=scontent.fybz2-2.fna&oh=20b788f19a225d99b3fd9b113febbcae&oe=5D33DED3" alt="Kaitlin Janvary" style="width:100%">
+      <div class="container">
+        <h2>Kaitlin Janvary</h2>
+        <p class="title">Events Chair</p>
+        <p>4th Year Life Sciences</p>
+      </div>
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">
+      <img src="https://scontent.fybz2-1.fna.fbcdn.net/v/t1.0-9/43610024_1970477566378282_8522471384608145408_n.jpg?_nc_cat=110&_nc_ht=scontent.fybz2-1.fna&oh=32323b6005863308da6f07ddb653cdf7&oe=5D40539F" alt="Shelby Empey" style="width:100%">
+      <div class="container">
+        <h2>Shelby Empey</h2>
+        <p class="title">Events Chair</p>
+        <p>3rd Year Environmental Science</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="https://scontent.fybz2-2.fna.fbcdn.net/v/t1.0-9/43518808_1970477583044947_4227032106062053376_n.jpg?_nc_cat=105&_nc_ht=scontent.fybz2-2.fna&oh=0fc8617e4140586d6d4c76983325aa9f&oe=5D33D453" alt="Susana Liu" style="width:100%">
+      <div class="container">
+        <h2>Susana Liu</h2>
+        <p class="title">Secretary</p>
+        <p>4th Year Chemistry</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="https://scontent.fybz2-1.fna.fbcdn.net/v/t1.0-9/43454528_1970477793044926_4401138367866077184_n.jpg?_nc_cat=110&_nc_ht=scontent.fybz2-1.fna&oh=e1d0aac6c9b50b9eb67b2bb34eb016fb&oe=5D341A9A" alt="Trinity Vey" style="width:100%">
+      <div class="container">
+        <h2>Trinity Vey</h2>
+        <p class="title">Departmental Affairs Chair</p>
+        <p>2nd Year Life Sciences</p>
+      </div>
+    </div>
+  </div>
+<div class="column">
+    <div class="card">
+      <img src="https://scontent.fybz2-1.fna.fbcdn.net/v/t1.0-9/43579708_1970477826378256_3576313512399470592_n.jpg?_nc_cat=111&_nc_ht=scontent.fybz2-1.fna&oh=0176a547f8474837b2760dd3bf3a2be3&oe=5D3D9E79" alt="Victoria Sa" style="width:100%">
+      <div class="container">
+        <h2>Victoria Sa</h2>
+        <p class="title">Departmental Affairs Chair</p>
+        <p>3rd Year Life Sciences</p>
+      </div>
+    </div>
+  </div>
+</div>
