60c32bbb53dd6211b8196fa37202da2d4826f5b8
Katie Lu <katie.yg.lu@gmail.com>
2019-04-07 18:22:50 -0400

    Update front-page.php

--- page-templates/front-page.php
+++ page-templates/front-page.php
@@ -12,7 +12,7 @@ get_header();
     <meta name="author" content="">
     <link rel="icon" href="../../../../favicon.ico">
 
-    <title>Jumbotron Template for Bootstrap</title>
+    <title>Queen's University Scientista</title>
 
     <!-- Bootstrap core CSS -->
     <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
@@ -25,44 +25,44 @@ get_header();
 
       <!-- Main jumbotron for a primary marketing message or call to action -->
       <div>
-<img src="https://lh3.googleusercontent.com/S-kuv9OwZm23IaFAhfC1ipODG1CqvUtWfix1Yc8mVdDPbNPKoVQECdeAeDuPY6TdlJzDweIBOaJ8w0nXWGSbD6bjrtC8NwAKeCHYsOL-QHLuRj3C5712MyvaTrbmWmm3_qjEg0P8" class="img-fluid" alt="Queen's U Scientista">
+<img src="https://lh3.googleusercontent.com/S-kuv9OwZm23IaFAhfC1ipODG1CqvUtWfix1Yc8mVdDPbNPKoVQECdeAeDuPY6TdlJzDweIBOaJ8w0nXWGSbD6bjrtC8NwAKeCHYsOL-QHLuRj3C5712MyvaTrbmWmm3_qjEg0P8" class="img-fluid" alt="Queen's U Scientista" style="width: 100%">
       </div>
        
 
-<div class="jumbotron" style="background-color: #6c4f99">
-        <div class="container" style="font-family: Montserrat; color:white;">
+<div class="jumbotron" style="background-color: white">
+        <div class="container" style="font-family: Montserrat; color:#6c4f99;">
           <h2 class="display-3" style="font-weight: bold;">About Us</h2>
           <p>Queen's University Scientista is the first Canadian Chapter of the <a href="http://www.scientistafoundation.com/" style="color: #d2d1ff; font-weight: bold;">Scientista Foundation</a> to be found in 2015.</p>
-          <p><a class="btn btn-primary btn-lg" href="./mission" role="button" style="background-color: white; color:#6c4f99">Learn More About Our Mission &raquo;</a></p>
+          <p><a class="btn btn-primary btn-lg" href="./mission" role="button" style="color: white; background-color:#6c4f99">Learn More About Our Mission &raquo;</a></p>
         </div>
       </div>
 
-    <div class="jumbotron" style="background-color: #d2d1ff">
+    <div class="jumbotron" style="background: linear-gradient(#6c4f99, #d2d1ff); color: white">
         <div class="container" style="font-family: Montserrat">
           <h2 class="display-3" style="font-weight: bold;">Podcast: Women of Scientista</h2>
           <p>"Women of Scientista" is a social media initiative with the goal of highlighting a women in STEMM on Queen's campus, including professors, researchers and graduate students.</p>
-          <p><a class="btn btn-primary btn-lg" href="https://www.podomatic.com/podcasts/queensscientista" style="background-color:000000" role="button">Listen here! &raquo;</a></p>
+          <p><a class="btn btn-primary btn-lg" href="https://www.podomatic.com/podcasts/queensscientista" style="background-color:#ffffff; color:#6c4f99" role="button">Listen here! &raquo;</a></p>
         </div>
       </div>
 
 
-   <div class="container">
+   <div class="container" style="font-family: Montserrat">
         <!-- Example row of columns -->
         <div class="row">
           <div class="col-md-4">
-            <h2>Podcast: Women of Scientista</h2>
-            <p>"Women of Scientista" is a social media initiative with the goal of highlighting a women in STEMM on Queen's campus, including professors, researchers and graduate students.</p>
-            <p><a class="btn btn-secondary" href="https://www.podomatic.com/podcasts/queensscientista" role="button">Listen here! &raquo;</a></p>
+            <h2>The Team</h2>
+            <p>Meet our Exec</p>
+            <p><a class="btn btn-secondary" href="./team" role="button">Learn More &raquo;</a></p>
           </div>
           <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            <h2>Gallery</h2>
+            <p>Discover our pictures from the Momentum: Discussion Panel Luncheon.</p>
+            <p><a class="btn btn-secondary" href="./gallery role="button">See More &raquo;</a></p>
           </div>
           <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            <h2>Contact Us</h2>
+            <p>Contact us with any questions!</p>
+            <p><a class="btn btn-secondary" href="./contact" role="button">Contact &raquo;</a></p>
           </div>
         </div>
 
@@ -73,7 +73,7 @@ get_header();
     </main>
 
     <footer class="container">
-      <p>&copy; Scientista 2017-2018</p>
+      <p>&copy; Scientista 2019</p>
     </footer>
 
     <!-- Bootstrap core JavaScript
@@ -87,4 +87,3 @@ get_header();
 </html>
 
 <?php get_footer(); ?>
-
