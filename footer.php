f472aad7d2ff421bf30f2ed3c5d952b374fcc044
Katie Lu <katie.yg.lu@gmail.com>
2019-04-07 14:32:24 -0400

    added social icons to footer

--- footer.php
+++ footer.php
@@ -1,6 +1,13 @@
-<h2>Footer Got Got</h2>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-</body>
-</html>
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet"/>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
+<footer class="footer">
+  <div class="container text-center">
+    <p>Contact Us!</p>
+    <a href="https://www.facebook.com/pg/scientistaqueens/"><i class="fa fa-facebook"></i></a>
+    <a href="https://www.instagram.com/scientista.queensu/"><i class="fab fa-instagram"></i></a>
+    <a href="mailto:scientista@clubs.queensu.ca"><i class="far fa-envelope"></i></a>
+  </div>
+</footer>
