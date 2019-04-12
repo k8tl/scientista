03908ae95962aac1594293a02f938408adfbbf75
Katie <17kygl@queensu.ca>
2019-04-12 15:02:22 -0400

    changed header

--- header.php
+++ header.php
@@ -3,6 +3,11 @@ global $template_settings, $post;
 ?>
 <!DOCTYPE html>
 <html>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet"/>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
 <head>
 <title><?php bloginfo('name'); ?> | <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, minimal-ui=0">
@@ -53,10 +58,22 @@ global $template_settings, $post;
         <li class="nav-item">
           <a class="nav-link" href="contact">Contact</a>
         </li>
-      </ul>
-      <form class="form-inline my-2 my-lg-0" style="align: right">
+	<li> 
+	<a href="https://www.facebook.com/pg/scientistaqueens/"><i class="fa fa-facebook"></i></a>
+	</li>
+	<li>
+	<a href="https://www.instagram.com/scientista.queensu/"><i class="fab fa-instagram"></i></a>
+	</li>
+	<li>
+	  <a href="mailto:scientista@clubs.queensu.ca"><i class="far fa-envelope"></i></a>
+	</li>
+	<li>
+	 <form class="form-inline my-2 my-lg-0" style="align: right">
         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
       </form>
+	</li>
+      </ul>
+     
     </div>
   </nav>
